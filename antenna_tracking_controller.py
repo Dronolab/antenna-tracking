ac7f2739b0012c47969e39f35de0e2638c50d9b3
Richer Larivière <richerlariviere@gmail.com>
2016-11-10 01:10:11 -0500

    Code is now compliant with PEP8

--- antenna_tracking_controller.py
+++ antenna_tracking_controller.py
@@ -1,11 +1,12 @@
+import json
+import math
 import socket
 import struct
-import math
 import Adafruit_PCA9685
-import json
+
 
 class AntennaTrackingController:
-    
+
     # Constants
     LISTENING_IP = "0.0.0.0"
     LISTENING_PORT = 5008
@@ -23,17 +24,18 @@ class AntennaTrackingController:
         self.pwm.set_pwm_freq(self.PMW_FREQUENCY)
 
     def start(self):
-        #Create socket
+        # Create socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         print "Socket created"
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         print "Socket binded"
         self.sock.bind((self.LISTENING_IP, self.LISTENING_PORT))
-        
+
         # Antenna coordinates
 
         #
-        # TODO: Quand on aura du GPS, faire du handling pour ne pas prendre la latitude et longitude par defaut
+        # TODO: Quand on aura du GPS, faire du handling pour ne pas prendre la
+        # latitude et longitude par defaut
         #
         self.antenna_latitude = self.SATELLITE_DISH_DEFAULT_LATITUDE
         self.antenna_longitude = self.SATELLITE_DISH_DEFAULT_LONGITUDE
@@ -48,13 +50,25 @@ class AntennaTrackingController:
             lat_drone = float(drone_gps['lat'])
             lon_drone = float(drone_gps['lon'])
             alt_drone = float(drone_gps['alt'])
-            
-            self.bearing_diff = self.bearing(self.antenna_latitude, self.antenna_longitude, lat_drone, lon_drone)
-            self.pitch_diff = self.pitch(self.antenna_latitude, self.antenna_longitude, self.antenna_altitude, lat_drone, lon_drone, alt_drone)
 
-	    print(self.bearing_diff)    
+            self.bearing_diff = self.bearing(
+                self.antenna_latitude,
+                self.antenna_longitude,
+                lat_drone, lon_drone
+            )
+
+            self.pitch_diff = self.pitch(
+                self.antenna_latitude,
+                self.antenna_longitude,
+                self.antenna_altitude,
+                lat_drone, lon_drone,
+                alt_drone
+            )
+
+        print(self.bearing_diff)
+
     #
-    # Gracefully stop antenna tracking controller 
+    # Gracefully stop antenna tracking controller
     #
     def stop(self):
         print("Antenna tracking stopped")
@@ -62,11 +76,11 @@ class AntennaTrackingController:
 
     def get_gpsdata(self):
         data, addr = sock.recvfrom(1024)
-        #print "gps_data:", data
+        # print "gps_data:", data
 
     def get_IMUdata(self):
         IMU_data, addr = sock.recvfrom(1024)
-        pitch,yaw,roll  = struct.unpack("ddd",IMU_data)
+        pitch, yaw, roll = struct.unpack("ddd", IMU_data)
 
     def bearing(self, lat_sat, long_sat, lat_drone, long_drone):
         lat_sat = math.radians(lat_sat)
@@ -75,16 +89,18 @@ class AntennaTrackingController:
         long_drone = math.radians(long_drone)
         delta_long = long_drone - long_sat
         delta_lat = lat_drone - lat_sat
-        y = math.sin(delta_long)*math.cos(lat_drone)
-        x = math.cos(lat_sat)*math.sin(lat_drone) - \
-        math.sin(lat_sat)*math.cos(lat_drone)*math.cos(delta_long)
-        #plage de -180 a 180
+        y = math.sin(delta_long) * math.cos(lat_drone)
+        x = math.cos(lat_sat) * math.sin(lat_drone) - \
+            math.sin(lat_sat) * math.cos(lat_drone) * math.cos(delta_long)
+        # plage de -180 a 180
         bearing_initial = math.degrees(math.atan2(y, x))
-        #Pour le mettre dans le plage de 0 a 360
-        #bearing_360=(bearing_initial+360)%360
+        # Pour le mettre dans le plage de 0 a 360
+        # bearing_360=(bearing_initial+360)%360
         return bearing_initial
-        
-    def pitch(self, lat_sat, long_sat,alt_sat, lat_drone, long_drone, alt_drone):
+
+    def pitch(self, lat_sat, long_sat, alt_sat, lat_drone, long_drone,
+              alt_drone):
+
         R = 6371000
         lat_sat = math.radians(lat_sat)
         lat_drone = math.radians(lat_drone)
@@ -92,32 +108,32 @@ class AntennaTrackingController:
         long_drone = math.radians(long_drone)
         delta_long = long_drone - long_sat
         delta_lat = lat_drone - lat_sat
-        delta_alt = alt_drone-alt_sat
-        a = math.pow(math.sin(delta_lat/2),2) + math.cos(lat_sat) * math.cos(lat_drone) * math.pow(math.sin(delta_long/2),2)
-        c = 2 * math.atan2(math.sqrt(a),math.sqrt(1-a))
+        delta_alt = alt_drone - alt_sat
+        a = math.pow(math.sin(delta_lat / 2), 2) + math.cos(lat_sat) * \
+            math.cos(lat_drone) * math.pow(math.sin(delta_long / 2), 2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
         d = R * c
-        pitch_angle = math.atan2(delta_alt,d)
+        pitch_angle = math.atan2(delta_alt, d)
         pitch_angle = math.degrees(pitch_angle)
-
-        return pitch_angle    
+        return pitch_angle
 
     def bearingoffset(self, angle, bearingangleoffset):
         newbearing = angle
         newbearing -= bearingangleoffset
-        if newbearing > 180 :
+        if newbearing > 180:
             newbearing -= 360
         if newbearing < -180:
-            newbearing +=360
+            newbearing += 360
 
         return newbearing
 
     def pitchoffset(self, angle, pitchangleoffset):
         newpitch = angle
         newpitch -= bearingangleoffset
-        if newpitch > 180 :
-       	    newpitch -= 360
+        if newpitch > 180:
+            newpitch -= 360
         if newbearing < -180:
-            newpitch +=360
+            newpitch += 360
 
         return newbearing
 
