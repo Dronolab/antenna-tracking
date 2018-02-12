868c42f566ddef4f09390d2dc1e9baca7a339698
jerrydark <garenajerry@gmail.com>
2018-02-12 12:30:26 -0500

    remade abstract_servo

--- Actuator/abstract_servo.py
+++ Actuator/abstract_servo.py
@@ -1,50 +1,88 @@
+import os
+import sys
+sys.path.append(os.getcwd()+"/../") #temporary solution for path
+from PID import PID
+import schedule
 import math
+from RPIO import PWM
+from Utility.abstract_process import processAbstract
+from Utility.MultiprocessDataType import *
 
 
-class AbstractServo():
+class AbstractServo(processAbstract):
 
-    def __init__(self, min_angle, max_angle, min_pwm, max_pwm,
-                 servo_frequency, channel):
-
-        self.delta = self._getDelta(
-            1,
-            self._pulse_from_pwm(max_pwm, servo_frequency),
-            -1,
-            self._pulse_from_pwm(min_pwm, servo_frequency))
-
-        self.hold_pwm = (max_pwm - min_pwm) / 2.0 + min_pwm
-        self.init = self._pulse_from_pwm(self.hold_pwm, servo_frequency)
-        self.current_angle = 0
-        self.desired_angle = 0
-        self.min_pwm = min_pwm
-        self.max_pwm = max_pwm
-        self.min_angle = min_angle
+    def __init__(self, antenna_shared_data, setpoint_shared_data, pin_number, min_angle, max_angle):
+        self.pid = PID()
+        self.pid.setSetPoint(0)
+        self.antenna_data = antenna_shared_data
+        self.setpoint_data = setpoint_shared_data
+        processAbstract.__init__(self)
+        self.pin_number = pin_number
         self.max_angle = max_angle
-        self.servo_frequency = servo_frequency
-        self.angle_tolerance = 0
-        self.channel = channel
+        self.min_angle = min_angle
+        #self.servo = PWM.Servo()
+
+    def refresh(self, delta):
+        feed_back_value = self.scale(delta, (-1*math.pi, math.pi), (-1, 1))
+        command = self.pid.update(feed_back_value)
+        pulse_width = self.pidscale(command)
+        self.servo.set_servo(self.pin_number, pulse_width)
+
+    def process(self):
+        self.servo = PWM.Servo()
+        schedule.every(0.015).seconds.do(self.job)
+        while True:
+            schedule.run_pending()
+
+    def job(self):
+        deltaYaw = self.setpoint_data.getYaw() - self.antenna_data.getYaw()
+        self.refresh(deltaYaw)
+
+    # scale value of in_range to out_range.
+
+    def scale(self, in_value, in_range, out_range):
+        if in_range[1] < in_range[0]:
+            in_max = in_range[0]
+            in_min = in_range[1]
+        else:
+            in_max = in_range[1]
+            in_min = in_range[0]
+        din = in_max - in_min
+
+        if out_range[1] < out_range[0]:
+            out_max = out_range[0]
+            out_min = out_range[1]
+        else:
+            out_max = out_range[1]
+            out_min = out_range[0]
+        dout = out_max - out_min
+
+        if in_value > in_max:
+            in_value = in_max
+        if in_value < in_min:
+            in_value = in_min
+        out_value = (((in_value - in_min) * dout) / din) + out_min
+        return out_value
 
 
-    def _getDelta(self, x1, y1, x2, y2):
-        slope = float(y2 - y1) / float(x2 - x1)
-        return slope
+    def pidscale(self, pid_in):
+        # object to change
+        if pid_in < -1:
+            pid_in = -1
+        elif pid_in > 1:
+            pid_in = 1
 
-    def _pulse_from_pwm(self, pwmvalue, pwmfrequency):
-        pulse = pwmvalue * 1000
-        pulse = pulse / 1000000
-        pulse = pulse * pwmfrequency
-        pulse = pulse * 4096
-        pulse = int(pulse)
-        return pulse
+        pid_range = (-1.0, 1.0)
+        # depends on servo used. experimental value.
+        servo_range = (1300, 1700)
+        pulse_w = self.scale(pid_in, pid_range, servo_range)
+        # pulse width increment must be by 10us
+        pulse_w = int(round(pulse_w, -1))
+        return pulse_w
 
-    def refresh(self, WantedAngle, CurrentAngle):
-        """ This method must be implemented by extending this class """
-
-        raise NotImplementedError()
-
-    def get_y(self, initval, slope, xval):
-        y = 0
-        y = slope * xval
-        y = y + initval
-        y = math.fabs(y)
-        return int(y)
+#usage
+#if __name__ == "__main__":
+#    ant = antenna_shared_data()
+#    point = setpoint_shared_data()
+#    s = ServoYaw(ant, point, [pin], [min_angle], [max_angle])
+#    s.start()
